cf94e8003cd61645e3d452e1f3234f15d916ef6b
Scott Upton <uptonic@gmail.com>
2012-04-03 20:18:47 -0600

    Get the navigation links working.

--- controller.rb
+++ controller.rb
@@ -5,3 +5,30 @@ layout /.*html.erb/ => 'layout.html.erb'
 
 # ignore files with a leading underline (ex: partials)
 ignore /\/_.*/
+
+before 'index.html.erb' do
+  @nav_home = true
+end
+before '/gallery/index.html.erb' do
+  @nav_gallery = true
+end
+
+before '/about/index.html.erb' do
+  @nav_about = true
+end
+
+before '/process/index.html.erb' do
+  @nav_process = true
+end
+
+before '/testimonials/index.html.erb' do
+  @nav_testimonials = true
+end
+
+before '/resources/index.html.erb' do
+  @nav_resources = true
+end
+
+before '/contact/index.html.erb' do
+  @nav_contact = true
+end
