e77e5e5ef494f403140cbef05ec4244b4f58928c
Scott Upton <uptonic@gmail.com>
2012-04-30 08:24:39 -0600

    Add galleries.

--- controller.rb
+++ controller.rb
@@ -7,28 +7,39 @@ layout /.*html.erb/ => 'layout.html.erb'
 ignore /\/_.*/
 
 before 'index.html.erb' do
+  @body_class = "home"
+end
+
+before 'index.html.erb' do
   @nav_home = true
 end
+
 before '/gallery/index.html.erb' do
+  @body_class = "gallery"
   @nav_gallery = true
 end
 
 before '/about/index.html.erb' do
+  @body_class = "about"
   @nav_about = true
 end
 
 before '/process/index.html.erb' do
+  @body_class = "process"
   @nav_process = true
 end
 
 before '/testimonials/index.html.erb' do
+  @body_class = "testimonials"
   @nav_testimonials = true
 end
 
 before '/resources/index.html.erb' do
+  @body_class = "resources"
   @nav_resources = true
 end
 
 before '/contact/index.html.erb' do
+  @body_class = "contact"
   @nav_contact = true
 end
