a459758ec54c21f79a4333e6ea6e51c009023c9e
Scott Upton <uptonic@gmail.com>
2012-09-12 23:48:10 -0600

    Updated styles for contact form.

--- public/javascripts/mailer.js
+++ public/javascripts/mailer.js
@@ -1,30 +1,40 @@
 $(function(){
-  
-  $("#email-form").submit(function(event) {
-  
-    // cancel event bubbling
+    
+  $("#email-form").submit(function(event) {    
+    // cancel event
     event.preventDefault();
-  
+    
     // empty the log and show the spinning indicator.
-  	var $log = $('#log').empty();
-	  
+  	var $log = $('#log').hide().empty();
+
 	  var $form = $(this),
       name      = $form.find('input[name="name"]').val(),
       email     = $form.find('input[name="email"]').val(),
       phone     = $form.find('input[name="phone"]').val(),
       comments  = $form.find('textarea[name="comments"]').val(),
       referral  = $form.find('select[name="referral"] option:selected').val(),
-      url       = $form.attr( 'action' );
+      url       = $form.attr('action');
 	  
+	  var $submit = $form.find('input[type="submit"]');
+	  var originalSubmitLabel = $submit.val();
+    
+    // Change the label on the submit button and disable it
+	  $submit.val('Submitting request...').attr('disabled', true);
+    
     $.post( url, { name: name, email: email, phone: phone, comments: comments, referral: referral },
       function(data) {
         // set log to PHP form response
-        $log.html(data).fadeIn();
+        $log.html(data).animate({
+          opacity: ['toggle', 'swing'],
+          height: ['toggle', 'swing']
+        }, 'fast');  
         
         // in case we successfully submitted the form
-        // disallow further submissions
+        // disallow further submissions        
         if ($log.find('div').hasClass('success')){
-          $('#email-form').bind('submit', function() { return false; });
+          $submit.css('visibility', 'hidden');
+        } else {
+          $submit.val(originalSubmitLabel).removeAttr('disabled');
         }
       }
     );
